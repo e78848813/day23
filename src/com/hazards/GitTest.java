ccded1a5e5c15791f012f923960c21af6ca69720
hazards <1027989868@qq.com>
2021-07-08 15:18:18 +0800

    新增了一条语句

--- src/com/hazards/GitTest.java
+++ src/com/hazards/GitTest.java
@@ -3,5 +3,6 @@ package com.hazards;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("新添加语句!");
     }
 }
